9d4ca9c98922dd1d17acf1984ae34c9e536327dc
Rahul <sitar@rocket-homepage.de>
2025-12-12 10:58:46 +0530

    Impresum update

--- src/app/(frontend)/impressum/page.js
+++ src/app/(frontend)/impressum/page.js
@@ -66,16 +66,9 @@ const page = () => {
         <div className="section">
           <h3>Haftung für Inhalte</h3>
           <p>
-            Als Diensteanbieter sind wir gemäß § 7 Abs. 1 TMG für eigene Inhalte auf diesen Seiten
-            nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als
-            Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde
-            Informationen zu überwachen oder nach Umständen zu forschen, die auf eine
-            rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der
-            Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine
-            diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten
-            Rechtsverletzung möglich. Bei Bekanntwerden entsprechender Rechtsverletzungen entfernen
-            wir diese Inhalte umgehend.
-          </p>
+            Als Diensteanbieter sind wir gemäß § 7 Abs. 1 DDG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Wir als Diensteanbieter sind jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt.
+            </p>
+            <p>Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.</p>
         </div>
 
         <div className="section">
